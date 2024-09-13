d040ff7117208d44819702807d3fffe554138d62
arvindajaybharadwaj <arvindajayarts@gmail.com>
2024-09-13 10:24:16 +0530

    Define state variables

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -1,7 +1,11 @@
 import HeadingContainer from './HeadingContainer';
 import ToDoListContainer from './ToDoListContainer';
+import { useState } from 'react';
 
 export default function MainContainer() {
+    const [tasksArray, setTasksArray] = useState([]); // initialize an empty array that contains all the tasks
+    const [task, setTask] = useState(""); // holds the current task
+
     return(
         <div className="main-container">
             <HeadingContainer />
