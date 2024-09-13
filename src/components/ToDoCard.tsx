54da16c0bfb8e10148dd890593fc5e5ebe473d1e
arvindajaybharadwaj <arvindajayarts@gmail.com>
2024-09-13 09:30:28 +0530

    Fix bug in clicking checkboxes

--- src/components/ToDoCard.tsx
+++ src/components/ToDoCard.tsx
@@ -1,9 +1,14 @@
-export default function ToDoCard() {
+interface toDoCardProps {
+    task_id: string;
+    task: string;
+} // type safety for passing props to ToDoCard component
+
+export default function ToDoCard({task_id, task}: toDoCardProps) {
     return(
         <div className="todo-card">
             <div className="main-task">
-                <input type="checkbox" id="task1" className="task-checkbox"></input>
-                <label htmlFor="task1" className="task-label">To Do</label>
+                <input type="checkbox" id={task_id} className="task-checkbox"></input>
+                <label htmlFor={task_id} className="task-label">{task}</label>
             </div>
             <div className="trash-button-container">
                 <button className="trash-button">
--- src/components/ToDoListContainer.tsx
+++ src/components/ToDoListContainer.tsx
@@ -3,10 +3,10 @@ import ToDoCard from './ToDoCard';
 export default function ToDoListContainer() {
     return(
         <div className="todo-list-container">
-            <ToDoCard />
-            <ToDoCard />
-            <ToDoCard />
-            <ToDoCard />
+            <ToDoCard task_id="task1" task="Go to the gym" />
+            <ToDoCard task_id="task2" task="Meditate for 10 mins" />
+            <ToDoCard task_id="task3" task="Code Todo List" />
+            <ToDoCard task_id="task4" task="4 hours of deep work" />
         </div>
     );
 }
